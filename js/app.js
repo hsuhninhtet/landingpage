b07646e6c460af9ac64e2fa5a52f801d9f223ad2
Su Hnin Htet <hsuhninhtet8@gmail.com>
2022-07-22 12:28:58 +0630

    landing page

---
+++ js/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    // START SHOWCASE SLIDER SECTION
+    $('.carousel').carousel({
+        interval:1000,
+        pause: 'hover'
+    }) 
+    // END SHOWCASE SLIDER SECTION 
+});
